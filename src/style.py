4c503e2607ff5ebf83fd6cccaf2e1164090394b3
Ruslan Popov <popov787ru@gmail.com>
2021-02-15 21:01:15 +0300

    Style.py part two

--- src/style.py
+++ src/style.py
@@ -1,264 +1,317 @@
-import sys
 import os
 
-
 USER_TYPES_ = [
-  'typedef',
-  'class',
-  'struct',
-  'union',
-  'enum'
+    'typedef',
+    'class',
+    'struct',
+    'union',
+    'enum'
 ]
-
 
 C_TYPES_ = [
-  'uint8_t',
-  'int8_t',
-  'uint16_t',
-  'int16_t',
-  'uint32_t',
-  'int32_t',
-  'uint64_t',
-  'int64_t'
+    'uint8_t',
+    'int8_t',
+    'uint16_t',
+    'int16_t',
+    'uint32_t',
+    'int32_t',
+    'uint64_t',
+    'int64_t'
 ]
-
 
 TYPES_ = [
-  'void',
-  'int',
-  'char',
-  'bool',
-  'signed',
-  'unsigned',
-  'short',
-  'long',
-  'float',
-  'double'
+    'void',
+    'int',
+    'char',
+    'bool',
+    'signed',
+    'unsigned',
+    'short',
+    'long',
+    'float',
+    'double'
 ]
-
 
 EXTENSION_ = [
-  '.cpp',
-  '.c',
-  '.m',
-  '.hpp',
-  '.h'
+    '.cpp',
+    '.c',
+    '.m',
+    '.hpp',
+    '.h'
 ]
 
-
 ERRORS_ = {
-  'FORMATTING': {},
-  'EXTENSION': {}
+    'FORMATTING': {},
+    'EXTENSION': {},
+    'HEADER_FILES': {},
+    'VARIABLE_DECLARATION': {},
+    'NAMESPACE': {},
+    'EXCEPTIONS': {},
+    'BASIC_DATA_TYPES': {},
 }
 
+OPERATORS_ = [
+    'while',
+    'for',
+    'if',
+]
 
-FOLDERS_ = []
-
-
-FLAG_ = False
-
-
-def insert_dict(dic: dict, pos, elem):
-  if len(dic) != 0 and pos in dic:
-    dic[pos].insert(0, elem)
-  else:
-    dic[pos] = [elem]
+def insert_dict(dic: dict, pos, elem, lines):
+        dic[pos] = {elem: lines}
 
 
-def print_errors(dic: dict):
-  print('============================')
-  for key1, value1 in dic.items():
-    print(key1)
-    for key2, value2 in value1.items():
-      print(key2, value2)
-  print('============================')
+def print_errors(dic: dict, file):
+    file_name = file.replace('//', '/')
+    if file.find('./src/') != -1:
+        file_name = file.replace('./src/', '')+ ' |'
+
+    for title, value in dic.items():
+        for line, errors in value.items():
+            print('\n============================================================')
+            print(file_name, line, end=' ')
+            for error, lines in errors.items():
+                print(error, '('+ title.lower() + ')', '\n' ,lines)
 
 
 def clear_errors(dic: dict):
-  for key, value in dic.items():
-    value.clear()
+    for key, value in dic.items():
+        value.clear()
 
 
 def is_func(line):
-  for type in TYPES_:
-    split_l = str(line).partition(' ')
-    if split_l[0] == type:
-      break
-  if str(line).find('(') != -1 and str(line).find(')'):
+    flag = False
+    for type in TYPES_:
+        split_l = str(line).partition(' ')
+        if split_l[0] == type:
+            flag = True
+            break
+    if flag:
+        if str(line).find('(') != -1 and str(line).find(')') != -1:
+            return True
+    return False
+
+
+def in_string(line: str, symbol : str):
+    if line.find('"') != -1:
+        pos_string = line.find('"')
+        if line.find(symbol) != -1 and \
+                line.find(symbol) > pos_string and \
+                line.find(symbol) < line.find('"', pos_string+1):
+            return True
+    return False
+
+
+def is_empty_line(line: str):
+    for char in line:
+        if not char.isspace():
+            return False
     return True
-  return False
 
 
 def make_header_guard(file, path):
-  if path.count('/') > 1:
-    path[path.find('/')].replace('/', '')
-  name = file.name.split('/')[-1]
-  return str('ENGINE' + path.replace('/', '_').replace('.', '').upper() + '_' + name.split('.')[0].upper()
-             + '_' + name.split('.')[1].upper() + '_')
+    newPath = path.replace('//', '/')
+    name = file.name.split('/')[-1]
+    return str('ENGINE' + newPath.replace('/', '_').replace('.', '').upper() + '_' + name.split('.')[0].upper()
+               + '_' + name.split('.')[1].upper() + '_')
 
 
 def text_formatting(file, path):
-  guard = ''
+    number = 1
+    indent = 0
+    last_indent = 0
+    last_line = ''
+    guard = ''
+    count_empty_line = 0
 
-  if file.name.find('.h') != -1:
-    guard = make_header_guard(file, path)
+    if file.name.find('.hpp') != -1:
+        guard = make_header_guard(file, path)
 
-  number = 1
-  last_line = str()
-  space = 0
+    for line in file:
+        hasOper = ''
+        current_indent = 0
+        typeInLine = ''
+        userType = ''
 
-  for line in file:
+        if is_empty_line(line):
+            count_empty_line += 1
 
-    if line.find('#ifndef') != -1 and file.name.find('.h') != -1 and line.split().pop() != guard:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '3.2')
+        if line.find('{') != -1 and line.find('namespace') == -1 and last_line.find('namespace') == -1:
+            indent += 1
+        elif line.find('}') != -1:
+            indent -= 1
 
-    if line.count(';') > 1 and line.find('for') == -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.1')
+        for type in TYPES_:
+            if line.find(type) != -1:
+                typeInLine = type
+                break
 
-    if is_func(line) and line.find('{') != -1 and line.find('}') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.2')
+        for type in USER_TYPES_:
+            if line.find(type) != -1:
+                userType = type
+                break
 
-    if line[len(line)-2] == ' ' or line[len(line)-2] == '\t':
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.3')
+        for operator in OPERATORS_:
+            if line.find(operator) != -1 and (line.find('#') == -1 or line.find('#') > line.find(operator)):
+                hasOper = operator
+                break
 
-    if line.find('{') != -1 and line.find('{') != space:
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.6')
+        for char in line:
+            if char == '\t':
+                current_indent += 1
+            elif char == ' ':
+                insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.11', last_line + line)
+                break
+            else:
+                break
 
-    if len(line) > 140:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.7')
+        if current_indent != indent and line.find('{') == -1 and not is_empty_line(line):
+            if current_indent > indent:
+                insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |',
+                            'Not enough whitespace',  last_line + line)
+            else:
+                insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |',
+                            'Too many whitespace', last_line + line)
 
-    if line.find('#define') != -1:
-      lst = line.replace('(', ' ').split()
-      if not lst[1].isupper():
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.8')
+        if line.find('#ifndef') != -1 and file.name.find('.hpp') != -1 and line.split()[1] != guard and number == 1:
+            insert_dict(ERRORS_.get('HEADER_FILES'), str(number) + ' line |', '3.2', last_line + line)
 
-    if line.find('#') != 0 and line.find('#') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.9')
+        if line.count(';') > 1 and line.find('for') == -1:
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.1', last_line + line)
 
-    if line.isspace() and last_line.isspace():
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.10')
+        if is_func(line) and line.find('{') != -1 and line.find('}') != -1:
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.2', last_line + line)
 
-    for char in line:
-      if char == ' ':
-        space += 1
-      elif char == '\t':
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.11')
-      else:
-        break
+        if line[len(line) - 2] == ' ' or line[len(line) - 2] == '\t':
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.3', last_line + line)
 
-    for type in TYPES_:
-      if line.find(type) < line.find('const') and line.find('const') != -1:
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.13')
-        break
+        if line.find('{') != -1 and line.find('{') != current_indent:
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.6', last_line + line)
 
-    for type in USER_TYPES_:
-      if line.find(type) != -1:
-        lst = line.split()
-        if not lst[1][0].isupper():
-          insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.15')
-          break
+        if len(line) > 140:
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.7', last_line + line)
 
-    if line.find('//') == 0:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.16')
+        if line.find('#define') != -1:
+            lst = line.replace('(', ' ').split()
+            if not lst[1].isupper():
+                insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.8', last_line + line)
 
-    if line.find('//') != -1 and last_line.find('//') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.17')
+        if current_indent != 0 and line[current_indent + 1] == '#':
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.9', last_line + line)
 
-    for type in TYPES_:
-      if line.find(type) != -1 and line.find('(') == -1:
-        lst = line.split()
-        if lst[1][0].isupper():
-          insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.19')
-          break
+        if count_empty_line > 1:
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.10', last_line + line)
+            count_empty_line = 0
 
-    if line.find('Float') != -1 and line.find('='):
-      digit = line.find('.') - 1
-      if not line[digit].isdigit():
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '1.20')
+        if line.find('const') != -1 and (line.find(typeInLine) < line.find('const')):
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.13', last_line + line)
 
-    if line.find('main') != -1 and is_func(line) and file.name.find('.m') == -1:
-      insert_dict(ERRORS_.get('EXTENSION'), 'extension', '2.2')
+        if len(userType) != 0:
+            lst = line.split()
+            if not lst[1][0].isupper():
+                insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.15', last_line + line)
 
-    for type in TYPES_:
-      if line.find(type) != -1 and (line.find('*') != -1 or line.find('&') != -1):
-        if line[line.find(type) + len(type)] != '*':
-          insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '5.5')
-          break
+        if line.find('//') != -1 and line.find('//') != last_indent and not in_string(line, '//'):
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.16', last_line + line)
 
-    for type in TYPES_:
-      if line.find(type) != -1 and not is_func(line) and line.find('=') == -1:
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '5.6')
-        break
+        if line.find('//') != -1 and not in_string(line, '//') and \
+                last_line.find('//') != -1 and not in_string(last_line, '//'):
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.17', last_line + line)
 
-    if line.find('using namespace') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '7.1')
+        if len(hasOper) != 0:
+            newLine = line.split('(')[1]
+            if newLine.find(')'):
+                newLine = newLine.replace(')', '')
+            if hasOper == 'for':
+                newLine = newLine.split(';')[0]
+        else:
+            newLine = line
 
-    if line.find('inline namespace') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '7.2')
+        if str(typeInLine) != 0 and not is_func(newLine) and \
+            newLine[newLine.find(typeInLine) + len(typeInLine)] == ' ' and \
+            newLine[newLine.find(typeInLine) + len(typeInLine) + 1].isupper():
+                insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.19', last_line + line)
 
-    if line.find('try') != -1 or line.find('throw') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '8.1')
+        if line.find('.') != -1 and line[line.find('.') + 1].isdigit() and not line[line.find('.') - 1].isdigit():
+            insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line |', '1.20', last_line + line)
 
-    if line.find('unsigned int') != -1:
-      insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '12.1')
+        if line.find('main') != -1 and is_func(line) and file.name.find('.m') == -1:
+            insert_dict(ERRORS_.get('EXTENSION'), 'extension |', '2.2', '')
 
-    for c_type in C_TYPES_:
-      if line.find(c_type) != -1:
-        insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '12.2')
-        break
+        if len(typeInLine) != 0 and line[line.find(typeInLine) + len(typeInLine)] == ' ' and \
+                ((line[line.find(typeInLine) + len(typeInLine) + 1] == '*') or
+                 (line[line.find(typeInLine) + len(typeInLine) + 1] == '&')):
+            insert_dict(ERRORS_.get('VARIABLE_DECLARATION'), str(number) + ' line |','5.5', last_line + line)
 
-    if line.find('sizeof(') != -1:
-      pos = line.find('sizeof(') + len('sizeof(')
-      for type in TYPES_:
-        if line.find(type) == pos:
-          insert_dict(ERRORS_.get('FORMATTING'), str(number) + ' line', '15.1')
-          break
+        if line.find('=') != -1 and not hasOper and \
+                ((len(typeInLine) != 0 and line.find(typeInLine) > line.find('=')) or len(typeInLine) == 0):
+            sep = line.split()
+            variable = sep[0]
+            if last_line.find(variable) != -1:
+                insert_dict(ERRORS_.get('VARIABLE_DECLARATION'), str(number) + ' line |', '5.6', last_line + line)
 
+        if line.find('using namespace') != -1:
+            insert_dict(ERRORS_.get('NAMESPACE'), str(number) + ' line |', '7.1', last_line + line)
 
-    last_line = line
-    number += 1
-    space = 0
+        if line.find('inline namespace') != -1:
+            insert_dict(ERRORS_.get('NAMESPACE'), str(number) + ' line |', '7.2', last_line + line)
 
-  if file.name.find('.h') != -1:
-    insert_dict(ERRORS_.get('EXTENSION'), 'extension', '2.1')
+        if line.find('try') != -1 or line.find('throw') != -1:
+            insert_dict(ERRORS_.get('EXCEPTIONS'), str(number) + ' line |', '8.1', last_line + line)
 
+        if line.find('unsigned int') != -1:
+            insert_dict(ERRORS_.get('BASIC_DATA_TYPES'), str(number) + ' line |', '12.1', last_line + line)
 
-def check_extension(name):
-  for ext in EXTENSION_:
-    if name.find(ext) != -1 and name.find("README") == -1:
-      return ext
-  return '.'
+        for c_type in C_TYPES_:
+            if line.find(c_type) != -1:
+                insert_dict(ERRORS_.get('BASIC_DATA_TYPES'), str(number) + ' line |', '12.2', last_line + line)
+        if not is_empty_line(line):
+            last_line = line
+        last_indent = current_indent
+        number += 1
+
+    if file.name.find('.h') != -1 and file.name.find('.hpp') == -1:
+        insert_dict(ERRORS_.get('EXTENSION'), 'extension |', '2.1', '')
 
 
-def check_files(paths):
-  dir = os.listdir(path=paths)
-  for file in dir:
-    ext = check_extension(str(file))
-    if ext != '.':
-      f = open(str(paths + '/' + file), 'r', encoding='utf-8')
-      text_formatting(f, paths)
-      f.close()
-      print('In ' + file + ' errors:')
+def find_valid_extension(name):
+    for ext in EXTENSION_:
+        if name.find(ext) != -1 and name.find("README") == -1:
+            return ext
+    return '.'
 
-      if ERRORS_:
-        FLAG_ = True
 
-      print_errors(ERRORS_)
-      clear_errors(ERRORS_)
+def check_files(paths, flag: bool):
+    dir = os.listdir(path=paths)
+    for file in dir:
+        ext = find_valid_extension(str(file))
+        if ext != '.':
+            f = open(str(paths + '/' + file), 'r', encoding='utf-8')
+            text_formatting(f, paths)
+            f.close()
+
+
+            if ERRORS_:
+                flag = True
+            if ERRORS_.get('EXTENSION') or ERRORS_.get('FORMATTING') :
+                print('In ' + file + ' errors:')
+                print_errors(ERRORS_, f.name)
+            clear_errors(ERRORS_)
 
 
 def main(paths: str):
+    flag = False
     dir = os.walk(paths)
     for current in dir:
-      paths = current[0]
-      if paths.find('./.') != 0:
-        for file in current[1]:
-          check_files(current[0] + '/' + file)
+        paths = current[0]
+        if paths.find('./.') != 0:
+            for folder in range(1, len(current) - 1):
+                for file in current[folder]:
+                    check_files(current[0] + '/' + file, flag)
 
-    if FLAG_ == False:
-      exit(1)
+    if not flag:
+        exit(1)
     else:
-      exit(0)
+        exit(0)
 
 
 if __name__ == '__main__':
